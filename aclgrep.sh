5f7eb6f8b9317ccfab9aeaecc19f5056cc1c2c5c
imshivamohan <87896804+imshivamohan@users.noreply.github.com>
2024-11-28 16:57:08 +0530

    Update aclgrep.sh

--- aclgrep.sh
+++ aclgrep.sh
@@ -31,3 +31,26 @@ awk '
 END {
     if (user) print user "," name "," (read ? read : "") "," (write ? write : "");
 }'
+
+##########################################
+kubectl exec -it kafka-0 -- kafka-acls --bootstrap-server kafka-service:9092 --list | \
+awk -v prefix="banking" '
+/ResourcePattern.*name=/ {
+    name = gensub(/.*name=([^,]+).*/, "\\1", "g");
+    if (index(name, prefix) == 1) {
+        matched = 1;
+        topic = name;
+    } else {
+        matched = 0;
+    }
+}
+/principal=/ && matched {
+    user = gensub(/.*principal=User:([^,]+).*/, "\\1", "g");
+    operation = gensub(/.*operation=([^,]+).*/, "\\1", "g");
+    if (operation == "READ") read = "read";
+    if (operation == "WRITE") write = "write";
+}
+matched && /principal=/ {
+    print user "," topic "," (read ? read : "") "," (write ? write : "");
+    read = write = "";
+}' > banking_acls.csv
